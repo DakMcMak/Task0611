1b2d4e7046de93a37b08eab68a8f05bbcbf0810f
DakMcMak <esbolov.galymzhan@mail.ru>
2017-04-26 22:51:49 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static String multiply(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i =0; i < 5; i++)
+            result.append(s);
+        return result.toString();
+    }
+
+    public static String multiply(String s, int count) {
+        StringBuilder result = new StringBuilder();
+        for (int i =0; i < count; i++)
+            result.append(s);
+        return result.toString();//напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
